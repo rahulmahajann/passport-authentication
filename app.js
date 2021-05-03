d73056674d4031c2705c1d707d55b6457bf8ca7e
rahul1237 <rahulmahajan98645@gmail.com>
2021-05-04 04:26:07 +0530

    added

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const expresslayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+
 const app = express()
+
+// database config
+const db = require('./config/keys').MongoURI
+
+// connect to mongoDB
+mongoose.connect(db, { useUnifiedTopology: true})
+    .then(() => console.log('mongoDB connected'))
+    .catch(err => console.log('bhag bsdk'))
 
 // ejs
 app.use(expresslayouts)
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://rahulmahajan98645@gmail.com:rahul98645!@cluster0.39vvf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
+
